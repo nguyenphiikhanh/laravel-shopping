5326baa157c1724ab97717ab92de4b714cf60554
unknown <nguyenphiikhanh@gmail.com>
2021-06-19 00:04:21 +0700

    feature : delete Category-method

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     //
+    use SoftDeletes;
     protected $fillable =[
         'name',
         'parent_id',
